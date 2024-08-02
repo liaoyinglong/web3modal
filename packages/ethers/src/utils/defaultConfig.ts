3136a04aaad50594d7b49a3a443770b09300b924
liaoyinglong <vigossliao@gmail.com>
2024-08-02 13:51:53 +0800

    disable coin base

--- packages/ethers/src/utils/defaultConfig.ts
+++ packages/ethers/src/utils/defaultConfig.ts
@@ -1,6 +1,6 @@
 import '@web3modal/polyfills'
 import type { Chain, Metadata, Provider, ProviderType } from '@web3modal/scaffold-utils/ethers'
-import { CoinbaseWalletSDK, type ProviderInterface } from '@coinbase/wallet-sdk'
+//import { CoinbaseWalletSDK, type ProviderInterface } from '@coinbase/wallet-sdk'
 import type { SocialProvider } from '@web3modal/scaffold-utils'
 
 export interface ConfigOptions {
@@ -39,7 +39,7 @@ export function defaultConfig(options: ConfigOptions) {
   let injectedProvider: Provider | undefined = undefined
 
   // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
-  let coinbaseProvider: ProviderInterface | undefined = undefined
+  //let coinbaseProvider: ProviderInterface | undefined = undefined
 
   const providers: ProviderType = { metadata }
 
@@ -62,42 +62,43 @@ export function defaultConfig(options: ConfigOptions) {
     return injectedProvider
   }
 
-  function getCoinbaseProvider() {
-    if (coinbaseProvider) {
-      return coinbaseProvider
-    }
+  //function getCoinbaseProvider() {
+  //  if (coinbaseProvider) {
+  //    return coinbaseProvider
+  //  }
 
-    if (typeof window === 'undefined') {
-      return undefined
-    }
+  //  if (typeof window === 'undefined') {
+  //    return undefined
+  //  }
 
-    const coinbaseWallet = new CoinbaseWalletSDK({
-      appName: metadata.name,
-      appLogoUrl: metadata.icons[0],
-      appChainIds: options.chains?.map(chain => chain.chainId) || [1, 84532]
-    })
+  //  const coinbaseWallet = new CoinbaseWalletSDK({
+  //    appName: metadata.name,
+  //    appLogoUrl: metadata.icons[0],
+  //    appChainIds: options.chains?.map(chain => chain.chainId) || [1, 84532]
+  //  })
 
-    /**
-     * Determines which wallet options to display in Coinbase Wallet SDK.
-     * @property options
-     *   - `all`: Show both smart wallet and EOA options.
-     *   - `smartWalletOnly`: Show only smart wallet options.
-     *   - `eoaOnly`: Show only EOA options.
-     * @see https://www.smartwallet.dev/sdk/v3-to-v4-changes#parameters
-     */
-    coinbaseProvider = coinbaseWallet.makeWeb3Provider({
-      options: options.coinbasePreference || 'all'
-    })
+  //  /**
+  //   * Determines which wallet options to display in Coinbase Wallet SDK.
+  //   * @property options
+  //   *   - `all`: Show both smart wallet and EOA options.
+  //   *   - `smartWalletOnly`: Show only smart wallet options.
+  //   *   - `eoaOnly`: Show only EOA options.
+  //   * @see https://www.smartwallet.dev/sdk/v3-to-v4-changes#parameters
+  //   */
+  //  coinbaseProvider = coinbaseWallet.makeWeb3Provider({
+  //    options: options.coinbasePreference || 'all'
+  //  })
 
-    return coinbaseProvider
-  }
+  //  return coinbaseProvider
+  //}
 
   if (enableInjected) {
     providers.injected = getInjectedProvider()
   }
 
   if (enableCoinbase) {
-    providers.coinbase = getCoinbaseProvider()
+    throw new Error('Coinbase is not supported yet')
+    //providers.coinbase = getCoinbaseProvider()
   }
 
   if (enableEIP6963) {
